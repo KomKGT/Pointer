f0164b3dd986ccfff0c6bf01234d7972b3696039
тайский человек <81642936+KomKGT@users.noreply.github.com>
2022-06-10 17:07:46 +0700

    Add files via upload

---
+++ Pointer.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+void recieverCHAR(int *pData)
+{
+	cout<<"recieverCHAR : "<<(char*)pData<<endl; 		//char
+}
+void recieverNUM(int *pData)
+{
+	cout<<"recieverNUM : "<<*pData<<endl; 					//num
+}
+int main()
+{
+	char *ptr = "KOMGRICH";
+	cout<<"*ptr : "<<*ptr<<endl; 	//output = K
+	cout<<" ptr : "<<ptr<<endl; 	//output = KOMGRICH
+	char buff[] = "Tranganpaiboon";
+	int Value = 10;
+	int *iptr = &Value;
+	cout<<*iptr<<endl;				//output = 10
+	recieverCHAR((int*)buff); 			//char
+	recieverCHAR((int*)ptr); 			//char
+	
+	recieverNUM((int*)iptr); 			//num
+	recieverNUM(&Value);				//same top		
+	
+}
